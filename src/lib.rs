2a125f264ef1810d26902fbd8e18ff2bb98f4196
Houtamelo <antoniopedrogf@hotmail.com>
2023-12-29 17:01:07 -0300

    Adding featured-gated modules to docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![feature(lazy_cell)]
 #![doc = include_str!("../README.md")]
+#![feature(doc_auto_cfg)]
 
 #[cfg(feature = "async_impl" )] pub mod async_impl;
 #[cfg(not(feature = "async_impl"))] pub mod sync_impl;

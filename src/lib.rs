20f87691a0758e26af7aaf1a198c76b55bd17ace
Houtamelo <antoniopedrogf@hotmail.com>
2023-12-29 18:03:46 -0300

    Updated documentation for docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![doc = include_str!("../README.md")]
-#![feature(doc_auto_cfg)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
+#[cfg_attr(docsrs, doc(cfg(feature = "async_impl")))]
 #[cfg(feature = "async_impl" )] pub mod async_impl;
+
 #[cfg(not(feature = "async_impl"))] pub mod sync_impl;
 
 pub mod utils;

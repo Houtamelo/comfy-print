eeeda2e815ae47963f9491256644b0b064ea7d5c
Houtamelo <antoniopedrogf@hotmail.com>
2023-12-29 22:28:06 -0300

    Migrating sync impl to separate package.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-#[cfg_attr(docsrs, doc(cfg(feature = "async_impl")))]
-#[cfg(feature = "async_impl" )] pub mod async_impl;
-
-#[cfg(not(feature = "async_impl"))] pub mod sync_impl;
-
+pub mod async_impl;
 pub mod utils;
 
